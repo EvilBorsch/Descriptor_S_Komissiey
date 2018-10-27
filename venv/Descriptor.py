81664ced3cc3e6213cfe14c02a066348b83103fa
EvilBorsch <pingvin1959@yandex.ru>
2018-10-27 16:07:47 +0300

    Добавил задание

--- venv/Descriptor.py
+++ venv/Descriptor.py
@@ -13,20 +13,26 @@ new_account.amount = 100
 print(new_account.amount)
 90
 """
+
+
 class Value:
-    def __get__(self, instance, owner):                             #что будет выводиться при запросе к ammount
+    def __get__(self, instance, owner):  # что будет выводиться при запросе к ammount
         return self.result
-    def __set__(self, instance, value):                             #что будет происходить если мы приравняем атрибут к чему-то, на вход принимается
-        self.result = value*(1-instance.commission)                 # класс атрибута и значение к которому мы приравниваем
+
+    def __set__(self, instance,
+                value):  # что будет происходить если мы приравняем атрибут к чему-то, на вход принимается
+        self.result = value * (1 - instance.commission)  # класс атрибута и значение к которому мы приравниваем
+
 
 class Account:
     def __init__(self, commission):
         self.commission = commission
-    amount = Value()                                                #создаем атрибут класса который мы будем вычислять в дескрипторе Value
+
+    amount = Value()  # создаем атрибут класса который мы будем вычислять в дескрипторе Value
 
 
-new_account = Account(0.3)                                          #создаём объект класса аккаунт и вводим комиссиб
-new_account.amount=100                                              #приравниваем атрибут объекта new_account класса Acount к какому-то значению
-print(new_account.amount)                                           #значение изменится т.к будет работать дескриптор value и его метод set
-new_account.amount=160                                              #повтор что точно всё хорошо работает
+new_account = Account(0.3)  # создаём объект класса аккаунт и вводим комиссиб
+new_account.amount = 100  # приравниваем атрибут объекта new_account класса Acount к какому-то значению
+print(new_account.amount)  # значение изменится т.к будет работать дескриптор value и его метод set
+new_account.amount = 160  # повтор что точно всё хорошо работает
 print(new_account.amount)
